56d3990300afaae4bdac8597bfc6197abbff1949
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-18 03:21:31 -0400

    programmatic navigation to the edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -28,5 +29,13 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    //Injecting Router to be able to use navigate method
+    //here we do not need an ID
+    // this.router.navigate(["edit"], { relativeTo: this.route });
+    //Here we go up one level and we need ID, this method for demo purposes
+    this.router.navigate(["../", this.id, "edit"], { relativeTo: this.route });
   }
 }
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  selector: "app-recipe-edit",
+  templateUrl: "./recipe-edit.component.html",
+  styleUrls: ["./recipe-edit.component.css"]
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    //retreving the ID
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      //checking if the user in the edit mode
+      this.editMode = params["id"] != null;
+      // console.log(this.editMode);
+    });
   }
-
 }
