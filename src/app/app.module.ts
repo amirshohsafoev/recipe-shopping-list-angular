d31843294e9d3d0d9f6b16b24cccf437e49177fa
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-20 20:33:30 -0400

    reactive form for recipe-edit && validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -31,7 +31,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpModule, ReactiveFormsModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
