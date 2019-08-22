05aa2bb38fac0cea579a2195f645ed6aed2203e1
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-22 16:36:54 -0400

    fetching and storing recipes on database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -32,7 +32,7 @@ import { RecipeService } from './recipes/recipe.service';
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [ShoppingListService,RecipeService],
   bootstrap: [AppComponent]
 })
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,25 +10,34 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>()
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      "Tasty Shnitzel",
-      "This is simply a test",
-      "https://cdn.pixabay.com/photo/2016/11/19/02/22/schnipo-1837703_960_720.jpg",
-      [(new Ingredient("Meat", 1), new Ingredient("French Fries", 20))]
-    ),
-    new Recipe(
-      "Big Fat Burger",
-      "This is simply a test",
-      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/5/5/0/FNM_060115-Fatbuger-Recipe_s4x3.jpg.rend.hgtvcom.826.620.suffix/1431449537270.jpeg",
-      [
-        (new Ingredient("Buns", 2),
-        new Ingredient("Meat", 4),
-        new Ingredient("Cheese", 8))
-      ]
-    )
-  ];
+  //My default recipes
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Tasty Shnitzel",
+  //     "This is simply a test",
+  //     "https://cdn.pixabay.com/photo/2016/11/19/02/22/schnipo-1837703_960_720.jpg",
+  //     [(new Ingredient("Meat", 1), new Ingredient("French Fries", 20))]
+  //   ),
+  //   new Recipe(
+  //     "Big Fat Burger",
+  //     "This is simply a test",
+  //     "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/5/5/0/FNM_060115-Fatbuger-Recipe_s4x3.jpg.rend.hgtvcom.826.620.suffix/1431449537270.jpeg",
+  //     [
+  //       (new Ingredient("Buns", 2),
+  //       new Ingredient("Meat", 4),
+  //       new Ingredient("Cheese", 8))
+  //     ]
+  //   )
+  // ];
+
+  private recipes: Recipe[] = []
+  
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes
+    this.recipeChanged.next(this.recipes.slice())
+  }
   getRecipes() {
     return this.recipes.slice();
   }
