b1d4340c5de90e1ba3d4e1a6895cfc84778a0dc7
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-25 17:45:44 -0400

    added some modules to reconstruct the scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,8 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit{
 
-  constructor(private authService: AuthService){}
+  constructor(
+    private authService: AuthService){}
 
   ngOnInit(){
     this.authService.autoLogin()
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
 import { Subscription } from 'rxjs';
+
 @Component({
   selector: "app-shopping-list",
   templateUrl: "./shopping-list.component.html",
